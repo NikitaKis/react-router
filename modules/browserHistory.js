3ca2e99a84afb70cd225f1edcd55119b81a57e63
Nikita <900940@mail.ru>
2016-12-06 14:41:12 +0400

    Update browserHistory.js

--- modules/browserHistory.js
+++ modules/browserHistory.js
@@ -1,4 +1,4 @@
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import createBrowserHistory from '../node_modules/history/lib/createBrowserHistory'
 import createRouterHistory from './createRouterHistory'
 export default createRouterHistory(createBrowserHistory)
 
