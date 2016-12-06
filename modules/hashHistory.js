34ad4709d79b162513e64c6c36cadc7ad9a12e3e
Nikita <900940@mail.ru>
2016-12-06 14:41:25 +0400

    Update hashHistory.js

--- modules/hashHistory.js
+++ modules/hashHistory.js
@@ -1,4 +1,4 @@
-import createHashHistory from 'history/lib/createHashHistory'
+import createHashHistory from '../node_modules/history/lib/createHashHistory'
 import createRouterHistory from './createRouterHistory'
 export default createRouterHistory(createHashHistory)
 
